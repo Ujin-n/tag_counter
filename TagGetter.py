e98a37c742ea082ff52937c1c9f4adf8c4633ec6
Yauheni Neshyn <Neshyn.Z@gmail.com>
2020-02-08 21:30:08 +0300

    Implemented logging

--- TagGetter.py
+++ TagGetter.py
@@ -1,5 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 class TagGetter:
@@ -11,19 +12,39 @@ class TagGetter:
 
     def run(self):
         """ Makes url request, parses html page, counts tags and prints them out. """
+        # verify url
         self.url_verification()
+
+        # url request
         html = urllib.request.urlopen(self.url).read()
 
+        # url log
+        self.url_logger()
+
+        # url parse
         soup = BeautifulSoup(html, 'html.parser')
         tags = soup.find_all()
 
+        # collect and count tags
         tag_dict = {}
-
         for tag in tags:
             tag_dict[tag.name] = tag_dict.get(tag.name, 0) + 1
 
         return tag_dict
 
     def url_verification(self):
+        """ Verify input URL, add https:// if needed. """
         if not self.url.lower().startswith('https://'):
             self.url = 'https://' + self.url
+
+    def url_logger(self):
+        """ Logs date, time and url. """
+        # getting date/time
+        now = datetime.now()
+        current_time = now.time().strftime("%H:%M:%S")
+        current_date = now.date()
+
+        # write to file
+        with open('log.txt', 'a') as f_reader:
+            f_reader.write(str(current_date) + ' ' + str(current_time) + ' ' + self.url + '\n')
+
